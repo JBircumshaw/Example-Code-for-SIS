b0e88401eda99374196347d72b8e479b5327d05a
JBircumshaw <100757430@unimail.derby.ac.uk>
2026-05-07 03:14:20 +0100

    spelling error correction

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ log1 = Log()
 log2 = Log()
 
 log1.create_log("Payment", "Customer payment completed")
-log2.create_log("Inqury", "Customer inquiry submitted")
+log2.create_log("Inquiry", "Customer inquiry submitted")
 
 print(log1.view_logs())
 print(log2.view_logs())
